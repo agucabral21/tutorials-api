14f01cf4af6bf483519e6f0b995ad7d2a0ad1598
agus <agucabral@gmail.com>
2023-02-06 18:39:56 -0300

    added basic User model

--- src/services/database.js
+++ src/services/database.js
@@ -1,11 +1,16 @@
 const Sequelize = require('sequelize');
+const { DataTypes } = require('sequelize');
+const UserModel = require('../models/User');
 const config = require('../config/db');
 
 const sequelize = new Sequelize(config);
+
+const User = UserModel(sequelize, DataTypes);
 
 // eslint-disable-next-line no-return-await
 (async () => await sequelize.sync({ alter: true }))();
 
 module.exports = {
   sequelize,
+  User,
 };
