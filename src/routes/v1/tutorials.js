9a8dd59998eb13453a7dd009c645d4fc6e2b9448
agus <agucabral@gmail.com>
2023-02-08 18:04:10 -0300

    feature : deleteById tutorial endpoint

--- src/routes/v1/tutorials.js
+++ src/routes/v1/tutorials.js
@@ -2,11 +2,12 @@ const router = require('express').Router();
 const TutorialController = require('../../controllers/v1/TutorialController');
 const { tokenValidation, tutorialTokenValidation } = require('../../middlewares/tokenValidation');
 const schemaValidate = require('../../middlewares/schemaValidate');
-const { add, findById, findAll } = require('../../validation/tutorial');
+const { add, findById, deleteById, findAll } = require('../../validation/tutorial');
 
 router.get('/token', tokenValidation, TutorialController.getToken);
 router.post('/', tutorialTokenValidation, schemaValidate(add), TutorialController.add);
 router.get('/', tokenValidation, schemaValidate(findAll), TutorialController.findAll);
 router.get('/:id', tokenValidation, schemaValidate(findById), TutorialController.findById);
+router.delete('/:id', tokenValidation, schemaValidate(deleteById), TutorialController.deleteById);
 
 module.exports = router;
