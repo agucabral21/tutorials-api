2c4cc71899425053c2a8f179ba7481d82fb793fa
agus <agucabral@gmail.com>
2023-02-08 13:23:48 -0300

    feature : valiation schemas for tutorial creation

--- src/routes/v1/tutorials.js
+++ src/routes/v1/tutorials.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const TutorialController = require('../../controllers/v1/TutorialController');
 const jwtValidation = require('../../middlewares/jwtValidation');
+const schemaValidate = require('../../middlewares/schemaValidate');
+const tutorialSchema = require('../../validation/tutorialSchema');
 
 router.use(jwtValidation);
 
 router.get('/token', TutorialController.getToken);
-router.post('/', TutorialController.add);
+router.post('/', schemaValidate(tutorialSchema), TutorialController.add);
 
 module.exports = router;
