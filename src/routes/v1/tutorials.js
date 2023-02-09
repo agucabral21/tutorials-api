b2cb82aec87c1f5dce85d519c839fda5d300fd8f
agus <agucabral@gmail.com>
2023-02-09 00:47:05 -0300

    feature : error handling middleware

--- src/routes/v1/tutorials.js
+++ src/routes/v1/tutorials.js
@@ -3,13 +3,14 @@ const TutorialController = require('../../controllers/v1/TutorialController');
 const { tokenValidation, tutorialTokenValidation } = require('../../middlewares/tokenValidation');
 const schemaValidate = require('../../middlewares/schemaValidate');
 const { add, put, findById, deleteById, findAll } = require('../../validation/tutorial');
+const catchAsync = require('../../errors/catchAsync');
 
-router.get('/token', tokenValidation, TutorialController.getToken);
-router.post('/', tutorialTokenValidation, schemaValidate(add), TutorialController.add);
-router.put('/:id', tokenValidation, schemaValidate(put), TutorialController.update);
-router.get('/', tokenValidation, schemaValidate(findAll), TutorialController.findAll);
-router.get('/:id', tokenValidation, schemaValidate(findById), TutorialController.findById);
-router.delete('/mass_delete', tokenValidation, TutorialController.massDelete);
-router.delete('/:id', tokenValidation, schemaValidate(deleteById), TutorialController.deleteById);
+router.get('/token', tokenValidation, catchAsync(TutorialController.getToken));
+router.post('/', tutorialTokenValidation, schemaValidate(add), catchAsync(TutorialController.add));
+router.put('/:id', tokenValidation, schemaValidate(put), catchAsync(TutorialController.update));
+router.get('/', tokenValidation, schemaValidate(findAll), catchAsync(TutorialController.findAll));
+router.get('/:id', tokenValidation, schemaValidate(findById), catchAsync(TutorialController.findById));
+router.delete('/mass_delete', tokenValidation, catchAsync(TutorialController.massDelete));
+router.delete('/:id', tokenValidation, schemaValidate(deleteById), catchAsync(TutorialController.deleteById));
 
 module.exports = router;
