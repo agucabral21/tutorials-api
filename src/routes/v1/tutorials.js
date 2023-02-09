60337e7637e8da081cb5c546bdc08a86c0629f41
agus <agucabral@gmail.com>
2023-02-09 15:51:07 -0300

    fix : renamed middleware to schemavalidation

--- src/routes/v1/tutorials.js
+++ src/routes/v1/tutorials.js
@@ -1,16 +1,16 @@
 const router = require('express').Router();
 const TutorialController = require('../../controllers/v1/TutorialController');
 const { tokenValidation, tutorialTokenValidation } = require('../../middlewares/tokenValidation');
-const schemaValidate = require('../../middlewares/schemaValidate');
+const schemaValidation = require('../../middlewares/schemaValidaton');
 const { add, put, findById, deleteById, findAll } = require('../../validation/tutorial');
 const catchAsync = require('../../errors/catchAsync');
 
 router.get('/token', tokenValidation, catchAsync(TutorialController.getToken));
-router.post('/', tutorialTokenValidation, schemaValidate(add), catchAsync(TutorialController.add));
-router.put('/:id', tokenValidation, schemaValidate(put), catchAsync(TutorialController.update));
-router.get('/', tokenValidation, schemaValidate(findAll), catchAsync(TutorialController.findAll));
-router.get('/:id', tokenValidation, schemaValidate(findById), catchAsync(TutorialController.findById));
+router.post('/', tutorialTokenValidation, schemaValidation(add), catchAsync(TutorialController.add));
+router.put('/:id', tokenValidation, schemaValidation(put), catchAsync(TutorialController.update));
+router.get('/', tokenValidation, schemaValidation(findAll), catchAsync(TutorialController.findAll));
+router.get('/:id', tokenValidation, schemaValidation(findById), catchAsync(TutorialController.findById));
 router.delete('/mass_delete', tokenValidation, catchAsync(TutorialController.massDelete));
-router.delete('/:id', tokenValidation, schemaValidate(deleteById), catchAsync(TutorialController.deleteById));
+router.delete('/:id', tokenValidation, schemaValidation(deleteById), catchAsync(TutorialController.deleteById));
 
 module.exports = router;
