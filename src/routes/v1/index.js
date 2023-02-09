620bbc68b9145d92b158ab41f0c2131219234cbb
agus <agucabral@gmail.com>
2023-02-09 13:09:29 -0300

    feature: added prepare endpoint to load test files

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,6 +4,7 @@ const errorHandler = require('../../middlewares/errorHandler');
 router.use('/status', require('./status'));
 router.use('/auth', require('./auth'));
 router.use('/tutorials', require('./tutorials'));
+router.use('/prep', require('./dbPrepare'));
 
 router.use(errorHandler);
 
