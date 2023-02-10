cbd474cf9c9e0dc4714b546af8d5a794053ac3c0
agus <agucabral@gmail.com>
2023-02-09 23:24:30 -0300

    minor changes

--- src/config/app.js
+++ src/config/app.js
@@ -5,13 +5,14 @@ dotenv.config();
 const express = require('express');
 const APIv1 = require('../routes/v1');
 const urlNotFoundHandler = require('../middlewares/urlNotFound');
+const errorHandler = require('../middlewares/errorHandler');
 
 const app = express();
 
 app.set('json spaces', 2);
 app.use(express.json());
-
 app.use('/api/v1', APIv1);
+app.use(errorHandler);
 
 // Response for every other route not specified
 app.all('*', urlNotFoundHandler);
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,11 +1,8 @@
 const router = require('express').Router();
-const errorHandler = require('../../middlewares/errorHandler');
 
 router.use('/status', require('./status'));
 router.use('/auth', require('./auth'));
 router.use('/tutorials', require('./tutorials'));
 router.use('/prep', require('./dbPrepare'));
-
-router.use(errorHandler);
 
 module.exports = router;
